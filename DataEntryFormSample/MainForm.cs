d2765f3ed87b62fdaf5b27f9384145e710cf33ef
Roman Tabunnikov <roman.tabunnikov@devexpress.com>
2019-07-31 16:27:59 +0300

    Add project files.

---
+++ DataEntryFormSample/MainForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataEntryFormSample {
+    public partial class MainForm : DevExpress.XtraBars.Ribbon.RibbonForm {
+        public MainForm() {
+            InitializeComponent();
+            LoadDocumentTemplate();
+        }
+
+        private void LoadDocumentTemplate() {
+            spreadsheetControl1.LoadDocument("PayrollCalculator_template.xlsx");
+        }
+    }
+}
